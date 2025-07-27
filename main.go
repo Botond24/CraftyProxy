c743c8314d14a2225bbb648dfcdd13d1ca9b84f7
Botond24 <44529287+Botond24@users.noreply.github.com>
2025-07-27 04:15:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	conf := getConfig()
-	c := crafty.New(conf.Addr, conf.Port, conf.Key, conf.Timeout)
+	c := crafty.New(conf.CraftyAddr, conf.Port, conf.Key, conf.Timeout)
 	c.GetServers()
 	var wg sync.WaitGroup
 	for _, server := range c.Servers {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			proxy.Handle(&server)
+			proxy.Handle(&server,conf.Addr)
 		}()
 	}
 	wg.Wait()
